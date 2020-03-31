0c76f2c2cd7a1d1e99e69cd3cf3d83254f6ceb0e
Subhasish Das <subhasishdas159@gmail.com>
2020-04-01 02:35:32 +0530

    PWA fixes

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,34 @@
+const version = "0.6.25";
+const cacheName = `airhorner-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/build/bundle.css`,
+        `/build/bundle.js`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+
 self.addEventListener('push', function(event) {
   event.waitUntil(self.registration.showNotification(event.data.text()));
 });
